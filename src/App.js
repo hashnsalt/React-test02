1d49d8a7425247d4124cbc7929fddb9451faf3b8
Eun-gyeong Lee <dyddydmmlee@gmail.com>
2023-01-21 11:07:34 +0900

    test08

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className='App'>
       <ul className='lnb'>
-        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/members'>Members</Link></li>
         <li><Link to='/shop'>Shop</Link></li>
       </ul>
       <hr />
       <Routes>
-        <Route path='home' element={<Home />} />
+        <Route path='' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='members/*' element={<Members />} />
         <Route path='shop' element={<Shop />} />
